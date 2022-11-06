dfb663b741e3cb3a3f2ad1aace5a59e26849fc09
mikab4 <mika.barkan4@gmail.com>
2022-11-06 12:10:59 +0200

    changed the make_so_file for updaet

--- make_so_file.bash
+++ make_so_file.bash
@@ -3,16 +3,14 @@ usage="$(basename "$0") [-h] [-l launch-file] [-b true|false] [-m true|false] [-
 -- this script runs menteebot light code
 
 where:
-    -p create candlePy.so to create pybind and also install with pip. No longer supported
     -h print usage
     "
 
 compile_py=0
 
-while getopts "ph:" flag
+while getopts "h:" flag
 do
     case "${flag}" in
-        p) compile_py=1;;
         h) echo "$usage"
           exit;;
         *) echo "usage: $0 [-r]" >&2
@@ -24,25 +22,13 @@ rm -rf build
 mkdir build
 cd build
 
-if [ $compile_py -eq 1 ]; then
-    echo "#############################Build pybind#################################33"
-    cmake .. -DCANDLE_BUILD_PYTHON=TRUE
-else
-    echo "#############################Build C++ library#################################33"
-    cmake ..
-fi
+echo "#############################Build C++ library#################################33"
+cmake ..
 
 make
 
-if [ $compile_py -eq 1 ]; then
-    echo "Installing pybind via pip"
-    cp pyCandle/pyCandle* ../candle_pip/src/mab/
-    cd ../candle_pip/
-    pip install .
-else
-    cp ../build/libcandle.so ../../ros2_workspace/candle_ros2/lib/
-    cp -r ../include/* ../../ros2_workspace/candle_ros2/include/Candle/
-fi
+cp ../build/libcandle.so ../../ros2_workspace/candle_ros2/lib/
+cp -r ../include/* ../../ros2_workspace/candle_ros2/include/Candle/
 
 echo "I am DONE"
 cd ..
